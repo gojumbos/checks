a276e33415370a82d25b04a23251fc277318d2ad
Declan Borcich <108373051+gojumbos@users.noreply.github.com>
2023-07-10 23:20:41 -0400

    Update __init__.py 05

--- secret_codes/__init__.py
+++ secret_codes/__init__.py
@@ -13,9 +13,25 @@ def runs():
     """ File runs without syntax errors """
     check50.py.compile("secret_codes.py")
 
+@check50.check(runs)
+def safe():
+    """ Main func prints Safe for bad input"""
+    out = check50.run("python secret_codes.py").stdin("[-1]").stdin("0").stdin("0").stdout()
+    if out != "Safe":
+        raise check50.Mismatch("Safe", out)
 
-check50.check(runs)
-def type_ret():
-    # imprt = check50.py.import_("secret_codes.py")
-    out = check50.run("python secret_codes.py").stdin([]).stdin(0).stdin(0).stdout()
-    print(out)
+
+@check50.check(runs)
+def check_1():
+    """ Main func prints correctly """
+    out = check50.run("python secret_codes.py").stdin("[0,1,2,3,4]").stdin("1").stdin("2").stdout()
+    if out != "[2,3]":
+        raise check50.Mismatch("[2,3]", out)
+
+@check50.check(runs)
+def check_2():
+    """ Main func prints correctly when signal is last character """
+    out = check50.run("python secret_codes.py").stdin("[5,7,6,8]").stdin("8").stdin("4").stdout()
+    if out != "[]":
+        raise check50.Mismatch("[2,3]", out)
+
