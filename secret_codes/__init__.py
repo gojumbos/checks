168c20820e2d71f95bbed67683ecf0c1b9dadd09
Declan Borcich <108373051+gojumbos@users.noreply.github.com>
2023-07-10 21:38:11 -0400

    Create __init__.py 01

---
+++ secret_codes/__init__.py
@@ -0,0 +1,21 @@
+
+import check50
+import check50.py
+
+
+@check50.check()
+def exists():
+    """ Check that secret_codes.py exists """
+    check50.exists("secret_codes.py")
+
+@check50.check(exists)
+def runs():
+    """ File runs without syntax errors"""
+    check50.py.compile("secret_codes.py")
+
+
+check50.check(runs)
+def has_main():
+    imprt = check50.py.import_("secret_codes.py")
+    if not imprt.main:
+        raise check50.Failure("Your program does not have a main() function")
